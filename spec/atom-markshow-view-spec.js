ff3baa5e3b696581ffa754e0d5dec648f9b45c57
BlankRain <731014656@qq.com>
2016-11-01 13:55:11 +0800

    init

---
+++ spec/atom-markshow-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomMarkshowView from '../lib/atom-markshow-view';
+
+describe('AtomMarkshowView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
